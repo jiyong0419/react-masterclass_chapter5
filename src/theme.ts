9b2c5232212864d920740736a5f1d3c76ecc0d3e
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-11-28 09:13:31 +0900

    #5.0
    React Router 설치, 사용법
    useParams() 사용

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  accentColor: "blue",
+};
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  accentColor: "yellow",
+};
