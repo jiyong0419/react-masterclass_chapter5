3c27c3444f6cc0967f7c2572652baebd94c231e8
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-11-28 16:22:34 +0900

    #5.6
    객체안에 수많은 property들을 type하기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -2,12 +2,77 @@ import { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
+interface ICoin {
+  coinId: string;
+}
+interface IState {
+  name: string;
+}
+interface ICoinInfo {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  tags: object;
+  team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object;
+  links_extended: object;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+interface IPriceInfo {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: number;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
   margin: 0 auto;
 `;
-
 const Header = styled.header`
   height: 10vh;
   display: flex;
@@ -18,24 +83,17 @@ const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
-
 const Loader = styled.div`
   text-align: center;
 `;
-
-interface ICoin {
-  coinId: string;
-}
-interface IState {
-  name: string;
-}
 
 function Coin() {
   const { coinId } = useParams<ICoin>();
   const [loading, setLoading] = useState(true);
   const { state } = useLocation<IState>();
-  const [coinInfo, setCoinInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [coinInfo, setCoinInfo] = useState<ICoinInfo>();
+  const [priceInfo, setPriceInfo] = useState<IPriceInfo>();
+
   useEffect(() => {
     (async () => {
       const coinData = await (
@@ -46,8 +104,12 @@ function Coin() {
       ).json();
       setCoinInfo(coinData);
       setPriceInfo(priceData);
+      console.log(coinData);
+      console.log(priceData);
+      setLoading(false);
     })();
   }, [coinId]);
+
   return (
     <Container>
       <Header>
