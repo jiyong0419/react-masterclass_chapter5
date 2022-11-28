f7214b9b8ab972a003bd5a513db999ba8fb3ce2e
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-11-28 13:37:19 +0900

    #5.5
    fetch 함수 축약해서 작성하기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -29,12 +29,25 @@ interface ICoin {
 interface IState {
   name: string;
 }
-interface ILocation {}
+
 function Coin() {
   const { coinId } = useParams<ICoin>();
   const [loading, setLoading] = useState(true);
   const { state } = useLocation<IState>();
-
+  const [coinInfo, setCoinInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const coinData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setCoinInfo(coinData);
+      setPriceInfo(priceData);
+    })();
+  }, [coinId]);
   return (
     <Container>
       <Header>
