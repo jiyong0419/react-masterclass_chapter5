4ddc2e2d5d54c376b20719570dcd826d6d99ce17
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-11-30 12:00:52 +0900

    #5.13
    APEXCHARTS로 Chart 표 만들기

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,14 +1,55 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import { fetchCoinChart } from "../api";
+import ApexChart from "react-apexcharts";
 
+interface IHistoricalData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 interface IChartProps {
   coinId: string;
 }
 function Chart({ coinId }: IChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
-    fetchCoinChart(coinId)
+  const { isLoading, data: priceData } = useQuery<IHistoricalData[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinChart(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          options={{
+            chart: {
+              height: 300,
+              width: 300,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            theme: { mode: "dark" },
+            stroke: { curve: "smooth", width: 4 },
+            grid: { show: false },
+            yaxis: { show: false },
+            xaxis: { labels: { show: false } },
+          }}
+          series={[
+            {
+              name: "Price",
+              data: priceData?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+        />
+      )}
+    </div>
+  );
 }
 export default Chart;
